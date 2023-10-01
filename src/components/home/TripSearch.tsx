ce58424199d26062369cf9e89448cf9ebad7754c
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-01 16:51:38 -0300

    feat: add shadcn components to home page

---
+++ src/components/home/TripSearch.tsx
@@ -0,0 +1,48 @@
+"use client";
+import CurrencyInput from "@/components/common/CurrencyInput";
+import DatePicker from "@/components/common/DatePicker";
+import Input from "@/components/common/Input";
+import { Button } from "@nextui-org/react";
+import React from "react";
+import worldMap from "@/../public/world-map.png";
+
+import { ImSearch } from "react-icons/im";
+import QuickSearch from "./QuickSearch";
+
+const TripSearch = () => {
+  return (
+    <section
+      className="container mx-auto p-5 text-center bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center"
+      style={{ backgroundImage: `url(${worldMap.src})` }}
+    >
+      <div className="hero">
+        <h2 className="font-bold text-xl md:text-2xl ">
+          Encontre o refúgio perfeito e{" "}
+          <span className="bg-gradient-to-r from-primaryHotefy-neutral to-primaryHotefy-lighter dark:from-primaryHotefy-neutral dark:to-primaryHotefy-lighter text-transparent bg-clip-text">
+            realize os seus sonhos!
+          </span>
+        </h2>
+        <p className="text-gray-600 mt-4 dark:text-white text-sm md:text-base max-w-2xl mx-auto">
+          Seja qual for o seu estilo de viagem, nós temos a acomodação ideal
+          para tranformar sua estadia em uma experiência única e memorável
+        </p>
+      </div>
+
+      <div className="flex flex-col md:flex-row items-center justify-center gap-2 my-4 w-full max-w-5xl mx-auto rounded-xl">
+        <Input placeholder="Qual é o seu destino?" />
+
+        <Button
+          color="secondary"
+          variant="shadow"
+          endContent={<ImSearch size={16} />}
+          className="w-full md:max-w-[10rem]"
+        >
+          Pesquisar
+        </Button>
+      </div>
+    </section>
+
+  );
+};
+
+export default TripSearch;
---
+++ src/components/home/TripTabs.tsx
@@ -0,0 +1,114 @@
+"use client";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { Sidebar } from "./SideBar";
+
+export default function TripTabs() {
+  return (
+    <>
+      <div className="md:block">
+        <div className="border-t">
+          <div className="bg-background dark:bg-[#18181b]">
+            <div className="grid lg:grid-cols-5">
+              <Sidebar className="hidden lg:block" />
+              <div className="col-span-3 lg:col-span-4 lg:border-l">
+                <div className="h-full px-4 py-6 lg:px-8">
+                  <Tabs defaultValue="all" className="h-full space-y-6">
+                    <div className="space-between flex items-center justify-center md:justify-start">
+                      <TabsList className="gap-1 px-1">
+                        <TabsTrigger value="all" className="relative">
+                          Todas
+                        </TabsTrigger>
+                        <TabsTrigger value="national" className="relative">
+                          Nacionais
+                        </TabsTrigger>
+                        <TabsTrigger value="international" className="relative">
+                          Internacionais
+                        </TabsTrigger>
+                      </TabsList>
+                    </div>
+                    {/* Todas */}
+                    <TabsContent
+                      value="all"
+                      className="border-none p-0 outline-none"
+                    >
+                      <div className="flex items-center justify-between">
+                        <div className="space-y-1">
+                          <h2 className="text-2xl font-semibold tracking-tight">
+                            Descubra sua Próxima Aventura
+                          </h2>
+                          <p className="text-sm text-muted-foreground">
+                            Aproveite uma ampla gama de opções de hospedagem
+                            cuidadosamente selecionadas, perfeitas para todos os
+                            tipos de viajantes.
+                          </p>
+                        </div>
+                      </div>
+                      <Separator className="my-4" />
+                      <div className="relative">
+                        <ScrollArea className="h-80 px-3 py-2">
+                          Inserir cards de todas as viagens aqui
+                          <ScrollBar orientation="horizontal" />
+                        </ScrollArea>
+                      </div>
+                    </TabsContent>
+                    {/* Nacionais */}
+                    <TabsContent
+                      value="national"
+                      className="border-none p-0 outline-none"
+                    >
+                      <div className="flex items-center justify-between">
+                        <div className="space-y-1">
+                          <h2 className="text-2xl font-semibold tracking-tight">
+                            Explore as Melhores Hospedagens no Brasil
+                          </h2>
+                          <p className="text-sm text-muted-foreground">
+                            Curta uma estadia de primeira classe em destinos
+                            exuberantes no Brasil.
+                          </p>
+                        </div>
+                      </div>
+                      <Separator className="my-4" />
+                      <div className="relative">
+                        <ScrollArea className="h-80 px-3 py-2">
+                          Inserir cards das viagens do brasil aqui
+                          <ScrollBar orientation="horizontal" />
+                        </ScrollArea>
+                      </div>
+                    </TabsContent>
+                    {/* Internacional */}
+                    <TabsContent
+                      value="international"
+                      className="border-none p-0 outline-none"
+                    >
+                      <div className="flex items-center justify-between">
+                        <div className="space-y-1">
+                          <h2 className="text-2xl font-semibold tracking-tight">
+                            Viagens de Luxo ao Redor do Mundo
+                          </h2>
+                          <p className="text-sm text-muted-foreground">
+                            Desfrute de estadias de luxo no melhor estilo
+                            internacional.
+                          </p>
+                        </div>
+                      </div>
+                      <Separator className="my-4" />
+                      <div className="relative">
+                        <ScrollArea className="h-80 px-3 py-2">
+                          Inserir cards das viagens internacionais aqui
+                          <ScrollBar orientation="horizontal" />
+                        </ScrollArea>
+                      </div>
+                    </TabsContent>
+                  </Tabs>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
