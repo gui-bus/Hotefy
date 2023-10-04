2b8eac62e3df9a91bc87f3de73117f5cc79ccc60
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-04 14:25:12 -0300

    feat: add recommended trips links

--- src/components/home/SideBar.tsx
+++ src/components/home/SideBar.tsx
@@ -22,18 +22,18 @@ const buttonData = [
 ];
 
 const recommendedData = [
-  { href: "/alianz", text: "Alianz Loft" },
-  { href: "/casa-proa", text: "Casa Proa" },
-  { href: "/dragon-temple", text: "Dragon Temple" },
-  { href: "/gladden-island", text: "Gladden Island" },
-  { href: "/mirror-house-north", text: "Mirror House North" },
-  { href: "/modern-oceanfront", text: "Modern Oceanfront" },
-  { href: "/ocaso-luxury-villa", text: "Ocaso Luxury Villa" },
-  { href: "/oceans-edge", text: "Ocean's Edge" },
-  { href: "/paradise", text: "Paradise" },
-  { href: "/whispering-pines", text: "Whispering Pines" },
-  { href: "/yui-valley", text: "Yui Valley" },
-  { href: "/zion-ecocabin", text: "Zion EcoCabin" },
+  { href: "/trips/8f6a1f51-1f8e-4a8c-8f03-37489f633f2b", text: "Alianz Loft" },
+  { href: "/trips/9b0f2817-ae92-4ff2-8d22-8478f0854bfc", text: "Casa Proa" },
+  { href: "/trips/b7219d9c-1988-4657-9e17-16a0fd58787a", text: "Dragon Temple" },
+  { href: "/trips/f11e9920-e78a-43d6-904b-3dc3b69e1f2b", text: "Gladden Island" },
+  { href: "/trips/6b67eaf7-f5a8-4415-85b1-a7b841b49cbb", text: "Mirror House North" },
+  { href: "/trips/aee3e85e-9480-4802-89ce-6178cc8fd01a", text: "Modern Oceanfront" },
+  { href: "/trips/990a0f8b-81d9-416b-aef4-ee8de36d9ffe", text: "Ocaso Luxury Villa" },
+  { href: "/trips/6b46d67c-a4de-4b79-9945-205cd293a866", text: "Ocean's Edge" },
+  { href: "/trips/93ea17ee-30ab-477c-a7c8-f4fa7da21c7d", text: "Paradise" },
+  { href: "/trips/8c04c98a-2eb9-4ac5-8731-594e0ebf5b20", text: "Whispering Pines" },
+  { href: "/trips/eef4f101-3977-48d7-b1c6-e0d2ef388f54", text: "Yui Valley" },
+  { href: "/trips/1ca333f1-0c57-43b7-adf6-65a0fbae4fc7", text: "Zion EcoCabin" },
 ];
 
 export function Sidebar({ className }: SidebarProps) {
