6a36001297670ff4aac9a1391cfb28370a7e3d40
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-04 01:09:18 -0300

    style: change tripitem footer color

--- src/components/common/TripItem.tsx
+++ src/components/common/TripItem.tsx
@@ -16,7 +16,6 @@ const TripItem = ({ trip }: TripItemProps) => {
   return (
     <Link href={`/trips/${trip.id}`}>
       <Card
-        isFooterBlurred
         isPressable
         radius="md"
         className="border-none w-80 mb-3 shadow-none select-none"
@@ -29,9 +28,9 @@ const TripItem = ({ trip }: TripItemProps) => {
           width={320}
           height={320}
         />
-        <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 py-2">
+        <CardFooter className="justify-between bg-[#18181b] border-white/20 border-1 overflow-hidden absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10 py-2">
           <div className="flex flex-col gap-1 items-center">
-            <p className="text-xs text-white font-semibold">{trip.name}</p>
+            <p className="text-xs text-white font-semibold z-50">{trip.name}</p>
 
             <div className="flex gap-1">
               <p className="text-xs text-white font-semibold">
@@ -40,7 +39,7 @@ const TripItem = ({ trip }: TripItemProps) => {
               <ReactCountryFlag countryCode={trip.countryCode} svg />
             </div>
           </div>
-          <div className="text-tiny text-white bg-black/20 px-4 py-2 rounded-full">
+          <div className="text-tiny text-white bg-secondary px-4 py-2 rounded-full font-medium">
             {parseFloat(price).toLocaleString("pt-BR", {
               style: "currency",
               currency: "BRL",
