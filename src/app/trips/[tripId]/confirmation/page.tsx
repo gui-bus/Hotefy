3954f16f2b158b461c5b9b37a2b4245cf2af5227
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-05 16:42:45 -0300

    feat: redirect the user on error in trip confirmation

--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -38,9 +38,14 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
           endDate: searchParams.get("endDate"),
         }),
       });
-      const { trip, totalPrice } = await response.json();
-      setTrip(trip);
-      setTotalPrice(totalPrice);
+
+      const res = await response.json();
+      if (res?.error) {
+        return router.push("/");
+      }
+
+      setTrip(res.trip);
+      setTotalPrice(res.totalPrice);
     };
 
     if (status === "unauthenticated") {
