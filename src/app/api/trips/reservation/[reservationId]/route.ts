cc0a0cf40de644202140e801f0af798804bd11f9
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-07 02:09:32 -0300

    fix: fix deploy

--- src/app/api/trips/reservation/[reservationId]/route.ts
+++ src/app/api/trips/reservation/[reservationId]/route.ts
@@ -1,15 +1,14 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function DELETE(
-  _request: Request,
-  { params: { reservationId } }: { params: { reservationId: string } }
-): Promise<Response> {
+export async function DELETE(_request: Request, { params: { reservationId } }: { params: { reservationId: string } }) {
   if (!reservationId) {
-    return new NextResponse(
-      JSON.stringify({ message: "Missing reservationId" }),
-      { status: 400 }
-    );
+    return {
+      status: 400,
+      body: {
+        message: "Missing reservationId",
+      },
+    };
   }
 
   const reservation = await prisma.tripReservation.delete({
