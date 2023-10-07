7be969d5204beda9603352770b2d00f67abbd415
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-10-07 02:23:04 -0300

    fix: fix deploy

--- src/app/api/trips/reservation/[reservationId]/route.ts
+++ src/app/api/trips/reservation/[reservationId]/route.ts
@@ -1,16 +1,47 @@
-import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { prisma } from '@/lib/prisma'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function DELETE(request: Request, { params: { reservationId } }: { params: { reservationId: string } }) {
+type paramsProps = {
+  params: { reservationId: string }
+}
+export async function DELETE(
+  _req: NextRequest,
+  { params: { reservationId } }: paramsProps,
+) {
+  console.log(reservationId)
   if (!reservationId) {
-    return new NextResponse(JSON.stringify({ message: "Missing reservationId" }), { status: 400 });
+    return new NextResponse(
+      JSON.stringify({
+        error: {
+          code: 'MISSING_RESERVATION_ID',
+        },
+      }),
+      {
+        status: 400,
+      },
+    )
   }
-
-  await prisma.tripReservation.delete({
+  const reservation = await prisma.tripReservation.findUnique({
+    where: { id: reservationId },
+  })
+  if (!reservation) {
+    return new NextResponse(
+      JSON.stringify({
+        error: {
+          code: 'RESERVATION_ID_NOT_FOUND',
+        },
+      }),
+      {
+        status: 404,
+      },
+    )
+  }
+  const reservationDeleted = await prisma.tripReservation.delete({
     where: {
       id: reservationId,
     },
-  });
-
-  return new NextResponse(JSON.stringify({ message: "Reservation deleted" }), { status: 200 });
+  })
+  return new NextResponse(JSON.stringify(reservationDeleted), {
+    status: 200,
+  })
 }
